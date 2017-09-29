30271484a77d0be6eafd8715b46a2a1f05c57393
Andrew Wan <andwan@gmail.com>
2017-09-29 19:22:53 +0100

    First files

---
+++ Node.Webkit/lib/appserver.js
@@ -0,0 +1,62 @@
+﻿"use strict";
+
+var url = require("url");
+
+const contentTextPlain = { "content-type": "text/pain" };
+const contentTextHtml = { "content-type": "text/html" };
+
+
+var QueryClass = class {
+    static getQueryItems(urlQuery) {
+        return url.parse(urlQuery, true).query();
+    }
+}
+
+var AppServer = class {
+    constructor() {
+        this.http = require("http");
+        this.init = false;
+        this.port = process.env.PORT || 1337;
+    }
+
+    begin() {
+        if (this.init) {
+            throw "Already initialised";
+        }
+        this.http.createServer((req, res) => {
+            try {
+                res.writeHead(200, contentTextHtml);
+
+                req.on("error", (err) => {
+                    console.error(err);
+                    res.statusCode = 400;
+                    res.end();
+                });
+
+                res.on("error",(err) => {
+                    console.error(err);
+                });
+
+                if (req.method === "GET" && req.url === "/test") {
+                    res.write("WORKING");
+                } else {
+                    res.writeHead(404, contentTextPlain);
+                    res.write("404 not found");
+                }
+
+
+            } catch (err) {
+                console.error()(err);
+            } finally {
+                res.end();
+                console.log("Request ended");
+            }
+
+        }).listen(this.port);
+        this.init = true;
+        console.log(`Initialised at ${Date()}`);
+        console.log(`Listening on port ${this.port}...`);
+    }
+}
+
+module.exports = AppServer;
--- Node.Webkit/server.js
+++ Node.Webkit/server.js
@@ -1,8 +1,6 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+const AppServer = require("./lib/appserver");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var server = new AppServer();
+server.begin();
+
